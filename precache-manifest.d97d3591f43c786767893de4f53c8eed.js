4086079df3ac26c3d4ee9d8d2fe0f29c564ff01c
PuntusYehor <maingithubacc@gmail.com>
2022-07-21 21:02:47 +0200

    Updates

---
+++ precache-manifest.d97d3591f43c786767893de4f53c8eed.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "1dbabb0ea6c7af0f946e",
+    "url": "/Todo/static/css/main.0d7870b5.chunk.css"
+  },
+  {
+    "revision": "1dbabb0ea6c7af0f946e",
+    "url": "/Todo/static/js/main.e9fea293.chunk.js"
+  },
+  {
+    "revision": "6fab73d75525f9d6064a",
+    "url": "/Todo/static/js/runtime~main.ee49a031.js"
+  },
+  {
+    "revision": "9605c783b61ea148c94a",
+    "url": "/Todo/static/css/2.1384bfc4.chunk.css"
+  },
+  {
+    "revision": "9605c783b61ea148c94a",
+    "url": "/Todo/static/js/2.365108eb.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Todo/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Todo/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Todo/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Todo/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Todo/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "92df2810943aa6cfe16782978db69206",
+    "url": "/Todo/index.html"
+  }
+];
